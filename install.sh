ce06407090da67d42960b86cb928dfc559c2be77
Sebastian Negomireanu <sebastian.negomireanu@logiccloud.de>
2024-03-06 21:21:45 +0200

    feat: Add Alacritty configuration files

--- install.sh
+++ install.sh
@@ -8,3 +8,8 @@ ln -s "$(pwd)/nvim" ~/.config/nvim
 # Setup tmux configuration
 rm -rf ~/.config/tmux
 ln -s "$(pwd)/tmux" ~/.config/tmux
+# Setup alacritty configuration
+rm -rf ~/.config/alacritty
+ln -s "$(pwd)/alacritty" ~/.config/alacritty
+
+
