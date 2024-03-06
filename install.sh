4ee926378e3bc744bbf3792f796c35ae7751dab0
Sebastian Negomireanu <sebastian.negomireanu@logiccloud.de>
2024-03-06 16:38:09 +0200

    chore: Add installation script and reorganize files

---
+++ install.sh
@@ -0,0 +1,6 @@
+rm -rf ~/.config/nvim
+ln -s /Users/seba/dev/nseba/nvim/nvim ~/.config/nvim
+brew install ripgrep
+brew tap homebrew/cask-fonts
+brew install --cask font-mononoki-nerd-font
+brew install luarocks
